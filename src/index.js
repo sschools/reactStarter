2651790e6799ed61f1f02838949d116b214623b1
Stephen Schools <stephen.schools>
2018-09-12 14:32:15 -0400

    saving to use remotely

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyBrN6tUWKyVzmuAG3rHbcwJeFCF3qGyQfQ';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+    YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <SearchBar/>
+        <VideoList videos={this.state.videos}/>
+      </div>
+    );
+  }
+}
+
+React.render(<App />, document.querySelector('.container'));
